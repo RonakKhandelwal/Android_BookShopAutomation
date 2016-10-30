052e54764afec8587d85f067bbd51611008b1252
RonakKhandelwal <ronak.khandelwal@learner.manipal.edu>
2016-10-30 18:52:29 +0530

    Activities designed

--- app/src/main/java/com/mad/crynoz/bookshop/DashBoard.java
+++ app/src/main/java/com/mad/crynoz/bookshop/DashBoard.java
@@ -1,13 +1,21 @@
 package com.mad.crynoz.bookshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class DashBoard extends AppCompatActivity {
+public class DashBoard extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        TextView read = (TextView)findViewById(R.id.read);
+        read.setOnClickListener(this);
     }
 }
